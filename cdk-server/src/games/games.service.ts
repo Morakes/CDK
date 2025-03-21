fac3ae9ad4ede895871c0ab6a80488414f48daba
wangjialong <1328735450@qq.com>
2025-03-21 16:45:06 +0800

    剔除大模型key

--- cdk-server/src/games/games.service.ts
+++ cdk-server/src/games/games.service.ts
@@ -12,7 +12,7 @@ export class GamesService {
   constructor(private prisma: PrismaService) {
     this.openai = new OpenAI({
       apiKey: process.env.OPENAI_API_KEY,
-      baseURL: 'https://dashscope.aliyuncs.com/compatible-mode/v1',
+      baseURL: '',
     });
   }
 
